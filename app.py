b289e33c0ba86d60603bea3a3d1376488e58febf
Femi <47614924+femid@users.noreply.github.com>
2022-04-03 14:46:32 -0700

    Add files via upload
    Python code files

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from datetime import datetime
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+
+# Get Today's Date in ISO 8601 format
+def get_date():
+    my_date = datetime.now()
+    today = my_date.strftime('%Y-%m-%d')
+    data = {"Today": today}
+
+    return json.dumps(data)
+
+
+
+
