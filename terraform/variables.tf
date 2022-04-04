fec8e480e1b85772c20527b2b24f53711645d0f7
Femi <47614924+femid@users.noreply.github.com>
2022-04-04 10:03:51 -0700

    Update variables.tf
    Updated description for repo_url variable. Added URL with list of supported Azure regions

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,25 +11,25 @@ variable "resource_group_name" {
 
 variable "location" {
   type = string
-  description = "Azure region to host resources. E.g. South Central US, UK South, West Europe, Southeast Asia"
+  description = "Azure region to host resources. E.g. South Central US, UK South, West Europe, etc. https://github.com/claranet/terraform-azurerm-regions/blob/master/REGIONS.md"
 }
 
 variable "appserviceplan_name" {
   type = string
-  description = "Name of the App service plan to host the Web App"
+  description = "Name of the App service plan to host the Web App. E.g AppServicePlan-B1"
 }
 
 variable "webapp_name" {
   type = string
-  description = "Unique name of Web app to host the site. E.g. WebApp-SuperUnique"
-}
-
-variable "repo_url" {
-  type = string
-  description = "GitHub repo URL that your GitHub Access Token can access. E.g 'https://github.com/femid/python-docs-hello-world'"
+  description = "Unique name of Web app to host the site. E.g. WebApp-FD"
 }
 
 variable "github_token" {
   type = string
   description = "Personal Access Token(PAT) with at least 'Read Public Repos' permission"
+}
+
+variable "repo_url" {
+  type = string
+  description = "GitHub repo URL that your GitHub PAT can access. E.g 'https://github.com/<YOUR GITHUB ACCOUNT>/simpleazurewebapp'"
 }
